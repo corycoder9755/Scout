ac880fe4b8eb6d9c0f242f183a89dca5312057ed
corycoder9755 <corycoderx@gmail.com>
2026-02-04 23:17:22 +0530

    Refactor address resolution loop for clarity

--- src/scout.c
+++ src/scout.c
@@ -24,28 +24,35 @@ struct addrinfo* translate_address(const char* argv[]) {
 
 int resolve_address(const char* argv[]) {
   struct addrinfo *response = translate_address(argv);
-
-  printf("Resolving the Hostname...\n");
   
-  while (response != NULL) {
+  printf("Resolving the Hostname...\n");
+
+  struct addrinfo *address = response;
+  
+  while (address != NULL) {
     char host[INET6_ADDRSTRLEN];
 
-    if (response->ai_family == AF_INET) {
-      struct sockaddr_in *ipv4 = (struct sockaddr_in *) &response->ai_addr;
+    if (address->ai_family == AF_INET) {
+      struct sockaddr_in *ipv4 = (struct sockaddr_in *) &address->ai_addr;
       inet_ntop(AF_INET, &(ipv4->sin_addr), host, INET6_ADDRSTRLEN);
     }
     else {
-      struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *) &response->ai_addr;
+      struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *) &address->ai_addr;
 
       inet_ntop(AF_INET6, &(ipv6->sin6_addr), host, INET6_ADDRSTRLEN);
     }
 
 
     printf("Address is: %s\n", host);
-    printf("Address Type is: %s\n", response->ai_family == AF_INET? "IPv4" : "IPv6");
-    printf("CNAME is: %s\n", response->ai_canonname);
-
-    response = response->ai_next;
+    printf("Address Type is: %s\n", address->ai_family == AF_INET? "IPv4" : "IPv6");
+    
+    if (address->ai_canonname != NULL) {
+      printf("CNAME is: %s\n\n", address->ai_canonname);
+    } else {
+      printf("\n\n");
+    }
+    
+    address = address->ai_next;
   }
 
   freeaddrinfo(response);
